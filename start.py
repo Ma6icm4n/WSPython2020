eae46f65d81b1cc8491184188ff83ec6cfae8b03
Magicman <clement.ciranna@viacesi.fr>
2020-11-04 10:56:07 +0100

    rapid change on standalone start

--- start.py
+++ start.py
@@ -2,11 +2,11 @@ import sys
 
 if r"C:\Users\Asus\Desktop\WSPython2020" not in sys.path:
     sys.path.append(r"C:\Users\Asus\Desktop\WSPython2020") #import QT lib
-    sys.path.append(r"C:\Users\Asus\Desktop\WSPython2020\Pipeline") #import path to project
+    sys.path.append(r"C:\Users\Asus\Desktop\WSPython2020\pipeline") #import path to project
 
 from Qt import QtWidgets, QtCompat, QtGui
-from Pipeline.ui import my_window as mw
-from Pipeline.engines import engine
+from pipeline.ui import my_window as mw
+from pipeline.engines import engine
 
 
 app = QtWidgets.QApplication(sys.argv)
